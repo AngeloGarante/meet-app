5801d0d79b8260bb144a2aa121eb2251c03be47d
AngeloGarante <angelogarante@outlook.com>
2023-02-20 12:14:53 +0100

    Updates

---
+++ precache-manifest.8ef2e80a77392ced03b66f9071423020.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "473193e54a0da926e56b",
+    "url": "/meet-app/static/css/main.f33ab317.chunk.css"
+  },
+  {
+    "revision": "473193e54a0da926e56b",
+    "url": "/meet-app/static/js/main.bfefedf5.chunk.js"
+  },
+  {
+    "revision": "66e278a9d9c001b8507d",
+    "url": "/meet-app/static/js/runtime~main.665eca24.js"
+  },
+  {
+    "revision": "984487ec12529b035376",
+    "url": "/meet-app/static/js/2.6167e07a.chunk.js"
+  },
+  {
+    "revision": "77ef5520517eb29546be",
+    "url": "/meet-app/static/js/3.fff3ce60.chunk.js"
+  },
+  {
+    "revision": "19b32827e1a104d0ed9d61b05b967368",
+    "url": "/meet-app/index.html"
+  }
+];
